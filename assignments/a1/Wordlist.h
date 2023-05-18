c1eb829f7f90e65ace830f6f4220a9f34fe2437b
Brandon Lee <brandonlee3241@hotmail.com>
2023-05-18 12:39:10 -0700

    Removed all of my own work for protection

--- assignments/a1/Wordlist.h
+++ assignments/a1/Wordlist.h
@@ -7,14 +7,14 @@
 // Student Info
 // ------------
 //
-// Name : <Brandon Lee>
-// St.# : <redacted>
-// Email: <redacted>
+// Name : <put your full name here!>
+// St.# : <put your full SFU student number here>
+// Email: <put your SFU email address here>
 //
 //
 // Statement of Originality
 // ------------------------
-
+//
 // All the code and comments below are my own original work. For any non-
 // original work, I have provided citations in the comments with enough
 // detail so that someone can see the exact source and extent of the
@@ -39,160 +39,38 @@ using namespace std;
 
 class Wordlist : public Wordlist_base
 {
+    //
+    // Use this Node to implement the singly-linked list for the word list. You
+    // can *add* extra variables/methods/features to this struct if you like,
+    // but you must keep its name the same, and also keep the word, count, and
+    // next variables as defined.
+    //
     struct Node
     {
         string word;
         int count;
         Node *next;
-        Node(const string& w, int c, Node* n) : word(w), count(c), next(n) {}
-
     };
-    
-    private:
-        Node* head; // head pointer 
-        string word; 
-    public:
-        Wordlist(){ // empty list constructor
-            head = nullptr;
-        }
 
-        Wordlist(string &filename){ //populate list with words from file
-            ifstream file(filename);
-            while (file >>word){
-                add_word(word);
-            }
-        }
+    //
+    // ... your code goes here ...
+    //
 
-        ~Wordlist(){ // destructor to delete all nodes in list
-            Node* tmp = head;
-            while(head){
-                tmp = head;
-                head = head->next;
-                delete(tmp);
-            }
-        }
+    //
+    // ... you can write helper methods if you need them ...
+    //
 
+    //
+    // ... write a default constructor that creates an empty Wordlist ...
+    //
 
-        //methods:
-        // The basis for most of these methods is to use one temporary node pointer to traverse the list 
-        // until we reach the desired location or node. Many of the methods will for the most part follow 
-        // the same structure, as most of the methods are a test of our ability to access data in a singly linked
-        // list, so I will only comment the first one in detail, and then will add comments 
-        // where there are differences.
-        // 
+}; // class Wordlist
 
-        
-        int get_count(const string &w)const override{
-            Node* p = head; //temp pointer to traverse the list
-            if(p == nullptr){ // checks if list is empty and returns 0 if so
-                return 0;
-            }
-            while(p->word != w){ // this will walk the pointer until we reach the desired word or the end of the list
-                p = p->next;
-                if(!p){ // if we reach the end of the list and the word is not found, return 0
-                    return 0;
-                }
-            }
-            return p->count;
-        }
-        
-        void add_word(const string &w)override{
-            Node* p = head; 
-            Node* tmp =nullptr; // tmp will be used to keep track of the node before the node we want to insert
-            if(p == nullptr){ // checks if list is empty, if so make a new node and set it as the head
-                head = new Node(w,1,nullptr);
-                return;
-            }
-            while(p!=nullptr && p->word.compare(w)<0){ // this will walk the pointer until we reach the correct spot to insert the word
-                tmp = p;
-                p = p->next;
-            }
-            if(p && p->word == w){ // if the word is already in the list, increment the count and return
-                p->count +=1;
-                return;
-            }
-            Node* newNode = new Node(w,1,p); 
-            if(tmp){ // if tmp is not null, we are not inserting at the head
-                tmp->next = newNode;
-            }
-            else{// if tmp is null, we are inserting at the head
-                head = newNode;
-            }
+//
+// ... you can write helper functions here (or before Wordlist) if you need them
+// ...
+//
 
-        }
-
-        string most_frequent()const override{
-            Node* p = head; 
-            Node* tmp = nullptr; //tmp will be used to keep track of the node with the highest count
-            int max=0; // max will be used to keep track of the highest count
-            while(p){
-                if(p->count > max){
-                    tmp = p;
-                    max = tmp->count;
-                }
-                p = p->next;
-            }
-            if(tmp){ // if tmp is not null, return the word with the highest count
-                return tmp->word;
-            }
-            else{
-                return ""; //returns null if list is empty;
-            }
-            
-        }
-
-        int num_different_words()const override{
-            int count=0; // initialize to 0 in case list is empty
-            Node* p = head;
-            while(p){ // walks the pointer to the end of the list and increments count
-                count+=1;
-                p = p->next;
-            }
-            return count;
-        }
-
-        int num_singletons()const override{
-            int count=0;
-            Node* p = head;
-            while(p){
-                if(p->count == 1){
-                    count+=1;
-                }
-                p = p->next;
-            }
-            return count;
-        }
-
-        bool is_sorted()const override{
-            Node* p = head;
-            while(p){
-                if(p->next && p->next->word < p->word){ // checks if the next word is less than the current word
-                    return false;
-                }
-                p = p->next;
-            }
-            return true;
-        }
-
-        int total_words()const override{
-            int count = 0;
-            Node* p = head;
-            while(p){
-                count+=p->count;
-                p = p->next;
-            }
-            return count;
-        }
-
-        void print_words()const override{
-            Node* p = head;
-            int count =0;
-            while(p){
-                count+=1;
-                cout << count << ". {"<<p->word<<", "<<p->count << "}"<<endl;
-                p = p->next;
-            }
-        }
-    
-
-}; 
+//
+// ... write functions to test every method of Wordlist ...
+//
--- assignments/a1/a1_main.cpp
+++ assignments/a1/a1_main.cpp
@@ -16,77 +16,77 @@
 //           Most frequent word: the 5437
 //         Number of singletons: 14919 (58%)
 //
-void test_read()
-{
-    Wordlist lst;
-    string w;
-    while (cin >> w)
-    {
-       // cout << "read word: " << w << endl;
-        lst.add_word(w);
-    }
+// void test_read()
+// {
+//     Wordlist lst;
+//     string w;
+//     while (cin >> w)
+//     {
+//        // cout << "read word: " << w << endl;
+//         lst.add_word(w);
+//     }
 
-    lst.print_stats();
-}
-
-
-//
-// test_wordlist() is a helper function that tests individual methods in the
-// Wordlist class. You should add your own tests for each method as you go. Call
-// it every time you run the program.
-//
-void test_wordlist2()
-{
-    cout << "test_wordlist() called ...\n";
-    Wordlist lst;
-    assert(lst.num_different_words() == 0);
-    assert(lst.total_words() == 0);
-    assert(lst.is_sorted());
-    assert(!lst.contains("hello"));
-
-    // ... add more tests: test all methods! ...
-
-    cout << "... test_wordlist() done: all tests passed!\n";
-}
-
-void testWordlist() {
-    Wordlist wordlist;
-
-    // Test adding words
-    wordlist.add_word("apple");
-    wordlist.add_word("banana");
-    wordlist.add_word("apple");
-    wordlist.add_word("orange");
-
-    // Test get_count
-    assert(wordlist.get_count("apple") == 2);
-    assert(wordlist.get_count("banana") == 1);
-    assert(wordlist.get_count("orange") == 1);
-    assert(wordlist.get_count("grape") == 0);
-
-    // Test most_frequent
-    assert(wordlist.most_frequent() == "apple");
-
-    // Test num_different_words
-    assert(wordlist.num_different_words() == 3);
-
-    // Test num_singletons
-    assert(wordlist.num_singletons() == 2);
-
-    // ... add more test cases for other methods ...
-    assert(wordlist.is_sorted());
-    assert(wordlist.total_words() == 4);
-    wordlist.print_words();
+//     lst.print_stats();
+// }
 
 
+// //
+// // test_wordlist() is a helper function that tests individual methods in the
+// // Wordlist class. You should add your own tests for each method as you go. Call
+// // it every time you run the program.
+// //
+// void test_wordlist2()
+// {
+//     cout << "test_wordlist() called ...\n";
+//     Wordlist lst;
+//     assert(lst.num_different_words() == 0);
+//     assert(lst.total_words() == 0);
+//     assert(lst.is_sorted());
+//     assert(!lst.contains("hello"));
 
-    cout << "All tests passed!" << endl;
-}
+//     // ... add more tests: test all methods! ...
+
+//     cout << "... test_wordlist() done: all tests passed!\n";
+// }
+
+// void testWordlist() {
+//     Wordlist wordlist;
+
+//     // Test adding words
+//     wordlist.add_word("apple");
+//     wordlist.add_word("banana");
+//     wordlist.add_word("apple");
+//     wordlist.add_word("orange");
+
+//     // Test get_count
+//     assert(wordlist.get_count("apple") == 2);
+//     assert(wordlist.get_count("banana") == 1);
+//     assert(wordlist.get_count("orange") == 1);
+//     assert(wordlist.get_count("grape") == 0);
+
+//     // Test most_frequent
+//     assert(wordlist.most_frequent() == "apple");
+
+//     // Test num_different_words
+//     assert(wordlist.num_different_words() == 3);
+
+//     // Test num_singletons
+//     assert(wordlist.num_singletons() == 2);
+
+//     // ... add more test cases for other methods ...
+//     assert(wordlist.is_sorted());
+//     assert(wordlist.total_words() == 4);
+//     wordlist.print_words();
+
+
+
+//     cout << "All tests passed!" << endl;
+//}
 
 int main()
 {
     //testWordlist();
-    test_wordlist2();
-    test_read();
+    //test_wordlist2();
+    //test_read();
     cout << "Welcome to assignment 1!\n";
 }
